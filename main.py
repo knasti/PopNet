6a834f09a4fbc8ab3ddfd9bc0e2736c1f0e6094b
Knasti / Niels Bach-Sørensen <nbs@dxt-dk.dk>
2018-02-08 19:09:22 +0100

    Removed null-values from np.array
    The null-values that follows in the tif-file are changed to zeros.

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import os
 import sys
 import numpy as np
+import tensorflow as tf
 from osgeo import gdal
+import osr
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(base_dir)
 data_dir = os.path.join(base_dir, 'data')
 
-ds = gdal.Open(os.path.join(data_dir, 'TGO14adjv1.tif'))
+pop_data = gdal.Open(os.path.join(data_dir, 'TGO14adjv1.tif'))
 
-myarray = np.array(ds.GetRasterBand(1).ReadAsArray())
+myarray = np.array(pop_data.GetRasterBand(1).ReadAsArray())
 print(myarray.shape)
 
 print(myarray.max())
+
+print(myarray)
+
+myarray[myarray < 0] = 0
+
